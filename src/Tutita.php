0856016c3849072ffad40c4ae33430e42656057a
T-ara <apple@MacBook-Pro.local>
2015-09-27 21:05:19 +0700

    add start

--- src/Tutita.php
+++ src/Tutita.php
@@ -3,6 +3,24 @@
 class Tutita{
 
     public function doSomething() {
-        return "action doSomething";
+        $nCol = 21;
+        $nRow = 5;
+        $nRow--;
+        $length = 2*$nRow;
+        $localIndex = array();
+        for($col = 0; $col < $nCol; $col++){
+            $localIndex[$col] = $col % $length;
+        }
+
+        for($row = 5; $row >= 0; $row = $row-1){
+            for($col=0; $col<$nCol; $col++){
+                if($localIndex[$col] == $row || $localIndex[$col] == $length - $row){
+                    print("*");
+                }else{
+                    print(" ");
+                }
+            }
+            print("\n");
+        }
     }
 }
